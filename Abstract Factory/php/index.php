81179f4d2d66d9b6d68b8a7602f6e706fb4fb477
Richard Lopes <contato.richardlopes@outlook.com>
2023-08-08 14:52:28 -0300

    refactor(abstract-factory): make the page more interactive

--- Abstract Factory/php/index.php
+++ Abstract Factory/php/index.php
@@ -6,27 +6,54 @@ require_once 'vendor/autoload.php';
  * NESSE EXEMPLO SIMULAMOS UM PROGRAMA QUE SUPORTA DIVERSOS TEMAS
  * A FUNÇÃO PRINCIPAL MAIN DEVE RECEBER UM ABSTRACT FACTORY
  * RESPONSÁVEL POR CRIAR OS COMPONENTES DA INTERFACE GRÁFICA
- * MAIS ABAIXO DUAS FÁBRICAS SÃO UTILIZADAS PARA EXEMPLIFICAR O USO
  */
 
-function main(\AbstractFactory\Interfaces\ThemeFactory $factory)
-{
+function main(\AbstractFactory\Interfaces\ThemeFactory $factory) {
+    global $themes;
+
     $container = $factory->makeContainer();
 
     $button = $factory->makeButton();
-    $button->setText('Clique aqui');
-    $button->onPress(function () use ($button) {
-        $class = explode('\\', get_class($button));
-        return "alert(\"Você clicou em um {$class[count($class) - 1]}\");";
+    $button->setText('Change');
+    $button->onPress(function () use ($themes) {
+        $index = array_search($_GET['theme'], $themes);
+        $next = $themes[$index + 1] ?? $themes[0];
+        return "window.location.href = \"?theme=$next\"";
     });
-    
+
     $container->add($button);
-    
+
     echo $container->render();
 }
 
-// Alterne entre as duas linhas abaixo para observar o funcionamento do pattern em ação
-$factory = new \AbstractFactory\Themes\Sky\SkyFactory();
-// $factory = new \AbstractFactory\Themes\Fashion\FashionFactory();
+/**
+ * APENAS UM HELPER PARA FACILITAR O REDIRECT
+ */
+
+function redirect($theme) {
+    header("Location: ?theme=$theme");
+    exit;
+}
+
+/**
+ * AQUI APENAS SELECIONAMOS A ABSTRACT FACTORY DE ACORDO COM O PARÂMETRO DA URL
+ */
+
+$factories = [
+    'Sky' => new \AbstractFactory\Themes\Sky\SkyFactory(),
+    'Fashion' => new \AbstractFactory\Themes\Fashion\FashionFactory(),
+    'Dark' => new \AbstractFactory\Themes\Dark\DarkFactory(),
+];
+
+$themes = array_keys($factories);
+
+!isset($_GET['theme']) && redirect($themes[0]);
+!isset($factories[$_GET['theme']]) && redirect($themes[0]);
+
+$factory = $factories[$_GET['theme']];
+
+/**
+ * POR ÚLTIMO CHAMAMOS A FUNÇÃO PRINCIPAL QUE RECEBER COMO PARÂMETRO A ABSTRACT FACTORY
+ */
 
 main($factory);
